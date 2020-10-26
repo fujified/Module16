3cd6bbb1bac78e9e8e5ebdcf0e38016f92cfb877
likeaspock <uktras@gmail.com>
2020-10-26 22:25:06 +0300

    Update Contents.swift

--- dz_16.playground/Contents.swift
+++ dz_16.playground/Contents.swift
@@ -37,7 +37,15 @@ humanTwo.name
 humanTwo.surname
 
 // 2 //
+let daysInMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-let daysInMonth = []
+for days in daysInMonth {
+    print(days)
+}
 
+let monthNames = ["January", "February", "March", "April", "May", "June", "Jule", "August", "September", "October", "November", "December"]
+
+for month in monthNames {
+    print("\(month): \(daysInMonth[0])")
+}
 // 3 //
